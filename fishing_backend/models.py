eea7d33f87b21e0747665e763c33bb1c78661565
Yonghai <dylanyonghaih@gmail.com>
2022-07-19 11:03:00 -0400

    add all fish data

--- fishing_backend/models.py
+++ fishing_backend/models.py
@@ -14,22 +14,10 @@ class Fish(models.Model):
     texture = models.TextField(null=True)
     species_illustration_photo = models.JSONField(null = True)
     image_gallery = models.JSONField(null = True)
+    caught = models.BooleanField(null=True)
 
     def __str__(self):
         return self.species_name
-
-# class Species_illustration_photo:
-#     fish = models.ForeignKey(Fish, on_delete=models.CASCADE, related_name="species_illustration_photo")
-#     src = models.TextField()
-#     alt = models.TextField()
-#     title = models.TextField()
-
-
-# class Image_gallery:
-#     fish = models.ForeignKey(Fish, on_delete=models.CASCADE, related_name="image_gallery")
-#     src = models.TextField()
-#     alt = models.TextField()
-#     title = models.TextField()
 
     
     
