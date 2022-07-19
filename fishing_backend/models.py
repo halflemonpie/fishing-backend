3254168c805d664f3d9cddaf0961a31fe21004ab
Yonghai <dylanyonghaih@gmail.com>
2022-07-19 16:35:59 -0400

    working

--- fishing_backend/models.py
+++ fishing_backend/models.py
@@ -14,7 +14,6 @@ class Fish(models.Model):
     texture = models.TextField(null=True)
     species_illustration_photo = models.JSONField(null = True)
     image_gallery = models.JSONField(null = True)
-    caught = models.BooleanField(null=True)
 
     def __str__(self):
         return self.species_name
