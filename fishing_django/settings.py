a9c4e9cac541272fd1a82b4b14bbff6353daad86
Yonghai <dylanyonghaih@gmail.com>
2022-07-20 01:38:16 -0400

    add new site for cors

--- fishing_django/settings.py
+++ fishing_django/settings.py
@@ -72,7 +72,7 @@ MIDDLEWARE = [
 
 ]
 
-CORS_ALLOWED_ORIGINS =  ["http://localhost:3000", 'http://127.0.0.1',]
+CORS_ALLOWED_ORIGINS =  ["http://localhost:3000", 'http://127.0.0.1', 'https://boat-fishing.netlify.app/']
 
 ROOT_URLCONF = 'fishing_django.urls'
 
